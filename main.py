192780f1a4885e700101811f991971180db6dfdc
Alexey <brech_36@mail.ru>
2023-09-12 15:30:03 +0900

    add task2 fix

--- main.py
+++ main.py
@@ -39,8 +39,8 @@
 # Ввод: print_operation_table(lambda x, y: x * y)
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
-    for i in a:
+    table = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in table:
         print(*[x for x in i])
 
 
