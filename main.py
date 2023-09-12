4f2ac5277b2e9dbffc002643883a5ee99ce9dc07
Alexey <brech_36@mail.ru>
2023-09-12 15:29:14 +0900

    add task2

--- main.py
+++ main.py
@@ -10,21 +10,38 @@
 #
 # **Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да
 #     **Вывод:** Парам пам-пам
-def rhythm(obj):
-    pharse_list = []
-    obj = obj.split()
-    for i in obj:
-        count = 0
-        for j in i:
-            if j == 'а':
-                count += 1
-        pharse_list.append(count)
-    return set(pharse_list)
+# def rhythm(obj):
+#     pharse_list = []
+#     obj = obj.split()
+#     for i in obj:
+#         count = 0
+#         for j in i:
+#             if j == 'а':
+#                 count += 1
+#         pharse_list.append(count)
+#     return set(pharse_list)
+#
+#
+#
+# pharse = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
+#
+# if len(rhythm(pharse)) == 1:
+#     print('Парам пам-пам')
+# else:print('Пам парам')
 
 
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и
+# столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
+# которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
+# почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
+# ровно два аргумента, как, например, у операции умножения
+# Ввод: print_operation_table(lambda x, y: x * y)
 
-pharse = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[x for x in i])
 
-if len(rhythm(pharse)) == 1:
-    print('Парам пам-пам')
-else:print('Пам парам')
+
+print_operation_table(lambda x, y: x * y)
